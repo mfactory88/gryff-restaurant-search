a29ab64bc647d99869efe031eee1f5eb4f4a357a
Inez Jimenez <ijim9182@yahoo.com>
2022-06-19 15:47:10 -0500

    topnav updated

--- js/script.js
+++ js/script.js
@@ -87,3 +87,13 @@ function searchRest () {
 };
 
 $('#search-button').on('click', searchRest);
+
+// media query with topnav
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += "responsive";
+    } else {
+        x.className = "topnav"
+    }
+}
